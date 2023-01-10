35d0e9b7f47b5de0cbb2d9d98e70604989e3cb23
santhosh <santhosh.kotha@hmcts.net>
2023-01-10 16:42:53 +0000

    Remove redundant ApplicationScoped annotation

--- framework-generators/rest-client-core/src/main/java/uk/gov/justice/services/clients/core/webclient/WebTargetFactoryFactory.java
+++ framework-generators/rest-client-core/src/main/java/uk/gov/justice/services/clients/core/webclient/WebTargetFactoryFactory.java
@@ -1,10 +1,8 @@
 package uk.gov.justice.services.clients.core.webclient;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
-@ApplicationScoped
 @Default
 public class WebTargetFactoryFactory {
 
